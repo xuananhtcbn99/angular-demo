58449ca53da89bd7cfc71021366c8a53aa1cbb94
nguyenthixuananh <nguyenthixuananh09061999@gmail.com>
2021-12-18 18:39:39 +0700

    bindingngIf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'Xuan Anh';
 
+  public age = 29;
+
   constructor() { }
 
   ngOnInit(): void {
