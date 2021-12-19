2431d1766a3fb9c65184d818c59fc4a0d8118919
nguyenthixuananh <nguyenthixuananh09061999@gmail.com>
2021-12-19 15:15:35 +0700

    pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { OnSalePipe } from './on-sale.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    OnSalePipe
   ],
   imports: [
     BrowserModule,
